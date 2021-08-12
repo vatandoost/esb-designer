e1dbcd409df576d1b9afcaedeb731ffe4fe20e83
masoud <masoudvatandoost1@gmail.com>
2021-08-12 10:24:51 +0430

    initialize esb

---
+++ database/migrations/2021_08_12_052317_create_project_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->uuid('id')->default(new Expression('uuid_generate_v4()'))->primary();
+            $table->uuid('main_ns_id');
+            
+            $table->string('name');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project');
+    }
+}
