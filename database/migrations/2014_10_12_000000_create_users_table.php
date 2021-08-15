72dceb07b9a74dbd0c0e2f619ce6d567490b5933
masoud <masoudvatandoost1@gmail.com>
2021-08-15 10:55:43 +0430

    add vue3 with inertia

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->default(new Expression('uuid_generate_v4()'))->primary();
+            $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
